2d56da623568aff1fb0ad4906a2623bc4df1871e
Smoked22 <smokedhernandez@gmail.com>
2021-10-20 21:30:31 -0300

    login required

--- Finanzas/views.py
+++ Finanzas/views.py
@@ -1,6 +1,8 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
 
 # Create your views here.
 
+@login_required
 def home_finanzas(request):
     return render(request, './home_finanzas.html')
