259fcadee0268a6b09ebd9106dc11d8f1137b9d7
Smoked22 <smokedhernandez@gmail.com>
2021-10-22 12:29:08 -0300

    Creacion App Cliente

---
+++ Cliente/urls.py
@@ -0,0 +1,9 @@
+from os import name
+from django.contrib import admin
+from django.urls import path
+
+
+urlpatterns = [
+    #Ejemplo:
+    #path('home/finanzassss', home_finanzas, name="home_finanzas"),
+]
