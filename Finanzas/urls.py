3d0509d976d0293eba68e9df01ae6884784778d8
Smoked22 <smokedhernandez@gmail.com>
2021-10-24 23:59:57 -0300

    path de boleta creado

--- Finanzas/urls.py
+++ Finanzas/urls.py
@@ -1,11 +1,12 @@
 from os import name
 from django.contrib import admin
 from django.urls import path
-from .views import home_finanzas
+from .views import *
 from django.conf.urls import url
 #from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = [
    path('home/', home_finanzas, name="homefinanzas"),
+   path('boletas/', boletas, name="boletas_finanzas"),
 ]
