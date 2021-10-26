ef526518335eee9833cf3aa380127dc190ac222c
Taniguchi21 <olovsdotterkm@gmail.com>
2021-10-26 18:18:22 -0300

    Crear Reserva y Mesa

--- Reserva/urls.py
+++ Reserva/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from django.contrib import admin
-from .views import home, reservas
+from .views import home, reservas, crearReserva, crearCliente
 
 urlpatterns = [
     path('home/', home, name="homeReserva"),
-    path('home/reservas', reservas, name="reservas")
+    path('home/reservas', reservas, name="reservas"),
+    path('home/crearReserva', crearReserva, name="crearReserva"),
+    path('home/crearCliente', crearCliente, name="crearCliente")
 ]
