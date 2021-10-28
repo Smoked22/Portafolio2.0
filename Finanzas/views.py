e13caab8a542c25a23d4b3e7c70438071a5f301f
Smoked22 <smokedhernandez@gmail.com>
2021-10-28 18:51:08 -0300

    funcione de mostrar proveedores y comentarios

--- Finanzas/views.py
+++ Finanzas/views.py
@@ -4,10 +4,12 @@ from django.db import connection
 
 # Create your views here.
 
+#creamos una funcion para renderizar la pagina home de finanzas
 @login_required
 def home_finanzas(request):
     return render(request, './home_finanzas.html')
 
+#Creamos una funcion para renderizar "registro_boletas.html"
 @login_required
 def boletas(request):
     data = {
@@ -16,6 +18,7 @@ def boletas(request):
     print(listado_boleta())
     return render(request, './registro_boletas.html', data)
 
+#Creamos una funciona para llamar al procedimiento almacenado
 def listado_boleta():
     django_cursor = connection.cursor()
     cursor = django_cursor.connection.cursor()
@@ -28,16 +31,32 @@ def listado_boleta():
         lista.append(fila)
     return lista
 
-#
-def total_boleta():
+#Funcion para renderizar la pagina "registro_proveedores.html"
+@login_required
+def proveedores(request):
+    data = {
+        'proveedores':listado_proveedor()
+    }
+    return render(request, './registro_proveedores.html',data)
+
+#Creamos una funciona para llamar al procedimiento almacenado
+def listado_proveedor():
     django_cursor = connection.cursor()
     cursor = django_cursor.connection.cursor()
     out_cur = django_cursor.connection.cursor()
 
-    cursor.callproc("SP_LISTAR_BOLETAS", [out_cur])
+    cursor.callproc("SP_LISTAR_PROVEEDORES", [out_cur])
 
     lista = []
     for fila in out_cur:
         lista.append(fila)
 
     return lista
+
+#Funcion para renderizar la pagina "guias_despacho.html"
+def guia_despacho(request):
+    return render(request, './guias_despacho.html')
+
+#Funcion para renderizar la pagina "guias_despacho.html"
+
+#codigo prueba
