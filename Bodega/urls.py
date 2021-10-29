70f96e2afe44172ba9dd84ddfe65b0664c4c64e3
rodrigontas <mati_x@hotmail.com>
2021-10-28 23:46:55 -0300

    hice los procedimientos almacenados para mostrar productos a bodega

--- Bodega/urls.py
+++ Bodega/urls.py
@@ -2,12 +2,14 @@ from os import name
 from django.contrib import admin
 from django.urls import path
 from .views import home
+from .views import *
 from django.conf.urls import url
 #from django.contrib.auth.decorators import login_required
 
 
 urlpatterns = [
    path('home/', home, name="home_bodega"),
+   path('ingredientes/', ingredientes, name="registro_ingrediente"),
    #path('', acceder, name="inicio"),
   
 ]
--- Bodega/views.py
+++ Bodega/views.py
@@ -6,14 +6,32 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import authenticate, login
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-
+from django.db import connection
 # Create your views here.
 
 @login_required
 def home(request):
     return render(request, './home_bodega.html')
+
+@login_required
+def ingredientes(request):
+    data = {
+        'ingredientes':listado_ingredienter()
+    }
     
-# Create your views here.
+    return render(request, './registro_ingrediente.html', data)
+
+def listado_ingredienter():
+    django_cursor = connection.cursor()
+    cursor = django_cursor.connection.cursor()
+    out_cur = django_cursor.connection.cursor()
+
+    cursor.callproc("SP_LISTAR_INGREDIENTER", [out_cur])
+
+    lista = []
+    for fila in out_cur:
+        lista.append(fila)
+    return lista
 
 
 
