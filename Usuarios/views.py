e115a20a87f39601f1a2543fdc284c5b748909e9
Smoked22 <smokedhernandez@gmail.com>
2021-11-04 11:52:21 -0300

    Inicios de sesion corregido

--- Usuarios/views.py
+++ Usuarios/views.py
@@ -78,19 +78,19 @@ def acceder(request):
             contra = form.cleaned_data.get("password")
             usuario = authenticate(username=nombre_usuario, password=contra)
             if usuario is None:
-                #login(request, usuario)
-                #return redirect("home")
                 messages.add_message(request, level=messages.ERROR, message="Usuario incorrecto")
             else:
                 login(request, usuario)
-                #switch (usuario.groups.filter)
-                #switch no existe en Python, asi que se utilisa elif :c
                 if usuario.groups.filter(name='Administrador').exists():
                     return redirect ("home")
                 elif usuario.groups.filter(name='Finanzas').exists():
                     return redirect ("homefinanzas")
                 elif usuario.groups.filter(name='Bodega').exists():
                     return redirect ("home_bodega")
+                elif usuario.groups.filter(name='Cocina').exists():
+                    return redirect ("home_cocina")
+                elif usuario.groups.filter(name='Reserva').exists():
+                    return redirect ("homeReserva")
         else:
             messages.add_message(request, level=messages.ERROR, message="Los datos son incorrectos")
 
