50d72eaa0432ebe75bc0816bad5b4d0c4a8a3c72
Taniguchi21 <olovsdotterkm@gmail.com>
2021-11-08 21:23:25 -0300

    PL SQL update

--- Finanzas/views.py
+++ Finanzas/views.py
@@ -5,20 +5,26 @@ import cx_Oracle
 
 # Create your views here.
 
-#creamos una funcion para renderizar la pagina home de finanzas
+# creamos una funcion para renderizar la pagina home de finanzas
+
+
 @login_required
 def home_finanzas(request):
     return render(request, './home_finanzas.html')
 
-#Creamos una funcion para renderizar "registro_boletas.html"
+# Creamos una funcion para renderizar "registro_boletas.html"
+
+
 @login_required
 def boletas(request):
     data = {
-        'boletas':listado_boleta()
+        'boletas': listado_boleta()
     }
     return render(request, './registro_boletas.html', data)
 
-#Creamos una funciona para llamar al procedimiento almacenado
+# Creamos una funciona para llamar al procedimiento almacenado
+
+
 def listado_boleta():
     django_cursor = connection.cursor()
     cursor = django_cursor.connection.cursor()
@@ -32,25 +38,26 @@ def listado_boleta():
     return lista
 
 
-#Funciona para renderizar la pagina "ingresar_boleta.html"
+# Funciona para renderizar la pagina "ingresar_boleta.html"
 @login_required
 def agegar_boleta(request):
     data = {
     }
-    #print(listado_boleta())
-    #ingresar_boleta('400','24-09-2021','14:20','12000','Transferencia','23')
-    #si es un metodo "POST" entra en el if
+    # print(listado_boleta())
+    # ingresar_boleta('400','24-09-2021','14:20','12000','Transferencia','23')
+    # si es un metodo "POST" entra en el if
     if request.method == 'POST':
         id_boleta = request.POST.get('id_boleta')
         fecha = request.POST.get('fecha')
         hora = request.POST.get('hora')
         monto = request.POST.get('monto')
         tipo_pago = request.POST.get('tipo_pago')
-        id_orden = request.POST.get('id_orden')  
+        id_orden = request.POST.get('id_orden')
 
-        #problema con el procedimiento almacenado, al momento de enviar los datos se queda cargando la pagina y ahi queda :C
-        #se cancela, si esta funcionando la wea, anoche tenai sueño el django y no queria funcionar
-        salida = ingresar_boleta(id_boleta, fecha, hora, monto, tipo_pago, id_orden)
+        # problema con el procedimiento almacenado, al momento de enviar los datos se queda cargando la pagina y ahi queda :C
+        # se cancela, si esta funcionando la wea, anoche tenai sueño el django y no queria funcionar
+        salida = ingresar_boleta(
+            id_boleta, fecha, hora, monto, tipo_pago, id_orden)
         if salida == 1:
             data['mensaje'] = 'agregado correctamente'
         else:
@@ -58,24 +65,31 @@ def agegar_boleta(request):
 
     return render(request, './ingresar_boleta.html', data)
 
-#Creamos una funcion para llamar el procedimiento almacenado
+# Creamos una funcion para llamar el procedimiento almacenado
+
+
 def ingresar_boleta(id_boleta, fecha, hora, monto, tipo_pago, id_orden):
     django_cursor = connection.cursor()
     cursor = django_cursor.connection.cursor()
     salida = cursor.var(cx_Oracle.NUMBER)
-    cursor.callproc('SP_AGREGAR_BOLETA2',[id_boleta, fecha, hora, monto, tipo_pago, id_orden, salida])
+    cursor.callproc('SP_AGREGAR_BOLETA2', [
+                    id_boleta, fecha, hora, monto, tipo_pago, id_orden, salida])
     return salida.getvalue()
 
-#Funcion para renderizar la pagina "registro_proveedores.html"
+# Funcion para renderizar la pagina "registro_proveedores.html"
+
+
 @login_required
 def proveedores(request):
     data = {
-        'proveedores':listado_proveedor()
+        'proveedores': listado_proveedor()
     }
-    #print(listado_proveedor())
-    return render(request, './registro_proveedores.html',data)
+    # print(listado_proveedor())
+    return render(request, './registro_proveedores.html', data)
 
-#Creamos una funciona para llamar al procedimiento almacenado
+# Creamos una funciona para llamar al procedimiento almacenado
+
+
 def listado_proveedor():
     django_cursor = connection.cursor()
     cursor = django_cursor.connection.cursor()
@@ -89,14 +103,18 @@ def listado_proveedor():
 
     return lista
 
-#Funcion para renderizar la pagina "Producto_proveedores.html"
-def listado_produc_proveedor(request,id):
-    data ={
-        'producto':lista_productos(id),
-    }
-    return render(request, './productos_proveedores.html',data)
+# Funcion para renderizar la pagina "Producto_proveedores.html"
 
-#Procedimiento almacenado "Listar productos de proveedores"
+
+def listado_produc_proveedor(request, id):
+    data = {
+        'producto': lista_productos(id),
+    }
+    return render(request, './productos_proveedores.html', data)
+
+# Procedimiento almacenado "Listar productos de proveedores"
+
+
 def lista_productos(rut):
     django_cursor = connection.cursor()
 
@@ -104,22 +122,26 @@ def lista_productos(rut):
 
     out_cur = django_cursor.connection.cursor()
 
-    cursor.callproc("SP_LISTAR_PRODUCTOS_PROVEEDORES", [out_cur,rut])
+    cursor.callproc("SP_LISTAR_PRODUCTOS_PROVEEDORES", [out_cur, rut])
 
     lista = []
     for fila in out_cur:
         lista.append(fila)
     return lista
 
-#Funcion para renderizar la pagina "guias_despacho.html"
+# Funcion para renderizar la pagina "guias_despacho.html"
+
+
 @login_required
 def guia_despacho(request):
     data = {
-        'guia_despacho':listado_guia_despacho
+        'guia_despacho': listado_guia_despacho
     }
     return render(request, './guias_despacho.html', data)
 
-#Procedimiento almacenado "Listar guias de despacho"
+# Procedimiento almacenado "Listar guias de despacho"
+
+
 def listado_guia_despacho():
     django_cursor = connection.cursor()
     cursor = django_cursor.connection.cursor()
@@ -133,5 +155,4 @@ def listado_guia_despacho():
     return lista
 
 
-
-#codigo prueba
+# codigo prueba
--- Reserva/views.py
+++ Reserva/views.py
@@ -13,18 +13,19 @@ from django.db import connection
 import cx_Oracle
 
 
-
-#Inicio Reservas
+# Inicio Reservas
 @login_required
 def home(request):
     data = {
-        #Almacena la variable para listar mesas
-        'mesas' : listado_mesas()
-       
-    }
-    return render(request, './home_reserva.html',data)
+        # Almacena la variable para listar mesas
+        'mesas': listado_mesas()
 
-#Inicio Reservas
+    }
+    return render(request, './home_reserva.html', data)
+
+# Inicio Reservas
+
+
 @login_required
 def pruebas(request):
     return render(request, './prueba.html')
@@ -33,180 +34,189 @@ def pruebas(request):
 def cliente_listado(request):
 
     data = {
-        'clientef' : listado_clientes(),
-        'cliente' : listado_clientes()
+        'clientef': listado_clientes(),
+        'cliente': listado_clientes()
     }
 
-    if request.method== 'POST':
+    if request.method == 'POST':
         id = request.POST.get('rutcli')
         listadofiltro = buscar_cliente(id)
         data['cliente'] = listadofiltro
 
-    return render(request, './cliente_listar.html',data)
+    return render(request, './cliente_listar.html', data)
 
 
-#Comenzar a hacer una reserva 
+# Comenzar a hacer una reserva
 @login_required
 def inicioReserva(request):
     return render(request, './cliente_listar.html')
 
-#Listar Mesas
+# Listar Mesas
+
+
 def mesas_listar(request):
 
     data = {
-        #Almacena la variable para listar mesas
-        'mesas' : listado_mesas()
+        # Almacena la variable para listar mesas
+        'mesas': listado_mesas()
     }
-    return render(request,'./mesas_listado.html',data)
+    return render(request, './mesas_listado.html', data)
 
 
 def listado_clientes():
     django_cursor = connection.cursor()
 
-    #Cursor que llama
+    # Cursor que llama
     cursor = django_cursor.connection.cursor()
-    #Cursor que recibe
+    # Cursor que recibe
     out_cur = django_cursor.connection.cursor()
 
-    #Llamada al cursor 
+    # Llamada al cursor
     cursor.callproc("SP_LISTAR_CLIENTES", [out_cur])
 
-    #llenamos la lista
-    lista= []
+    # llenamos la lista
+    lista = []
     for fila in out_cur:
         lista.append(fila)
-    return lista 
+    return lista
 
-#Procedimiento Listar
+# Procedimiento Listar
+
+
 def listado_mesas():
     django_cursor = connection.cursor()
 
-    #Cursor que llama
+    # Cursor que llama
     cursor = django_cursor.connection.cursor()
-    #Cursor que recibe
+    # Cursor que recibe
     out_cur = django_cursor.connection.cursor()
 
-    #Llamada al cursor 
-    cursor.callproc("SP_LISTAR_MESAS_DISPONIBLES_AHORA", [out_cur])
+    # Llamada al cursor
+    cursor.callproc("SP_LISTAR_MESAS_DISPONIBLES", [out_cur])
 
-    #llenamos la lista
-    lista= []
+    # llenamos la lista
+    lista = []
     for fila in out_cur:
         lista.append(fila)
-    return lista 
+    return lista
+
 
 def listado_empleados():
     django_cursor = connection.cursor()
 
-    #Cursor que llama
+    # Cursor que llama
     cursor = django_cursor.connection.cursor()
-    #Cursor que recibe
+    # Cursor que recibe
     out_cur = django_cursor.connection.cursor()
 
-    #Llamada al cursor 
+    # Llamada al cursor
     cursor.callproc("SP_LISTAR_EMPLEADOS", [out_cur])
 
-    #llenamos la lista
-    lista= []
+    # llenamos la lista
+    lista = []
     for fila in out_cur:
         lista.append(fila)
-    return lista 
+    return lista
+
 
 def listado_mesas_disponibles():
     django_cursor = connection.cursor()
-    #Cursor que llama
+    # Cursor que llama
     cursor = django_cursor.connection.cursor()
-    #Cursor que recibe
+    # Cursor que recibe
     out_cur = django_cursor.connection.cursor()
 
-    #Llamada al cursor 
+    # Llamada al cursor
     cursor.callproc("SP_LISTAR_MESAS_DISPONIBLES", [out_cur])
 
-    #llenamos la lista
-    lista= []
+    # llenamos la lista
+    lista = []
     for fila in out_cur:
         lista.append(fila)
-    return lista 
+    return lista
+
 
 def listado_reservas_por_RUT(id):
     django_cursor = connection.cursor()
 
-    #Cursor que llama
+    # Cursor que llama
     cursor = django_cursor.connection.cursor()
-    #Cursor que recibe
+    # Cursor que recibe
     out_cur = django_cursor.connection.cursor()
 
-    #Llamada al cursor 
+    # Llamada al cursor
     cursor.callproc("SP_BUSCAR_RESERVA_POR_RUTCLI", [out_cur, id])
 
-    #llenamos la lista
-    lista= []
+    # llenamos la lista
+    lista = []
     for fila in out_cur:
         lista.append(fila)
-    return lista 
+    return lista
 
 
 def buscar_reserva(id):
     django_cursor = connection.cursor()
 
-    #Cursor que llama
+    # Cursor que llama
     cursor = django_cursor.connection.cursor()
-    #Cursor que recibe
+    # Cursor que recibe
     out_cur = django_cursor.connection.cursor()
 
-    #Llamada al cursor 
-    cursor.callproc("SP_BUSCAR_RESERVA", [out_cur,id])
+    # Llamada al cursor
+    cursor.callproc("SP_BUSCAR_RESERVA", [out_cur, id])
 
-    #llenamos la lista
-    lista= []
+    # llenamos la lista
+    lista = []
     for fila in out_cur:
         lista.append(fila)
-    return lista 
+    return lista
+
 
 def buscar_cliente(id):
     django_cursor = connection.cursor()
 
-    #Cursor que llama
+    # Cursor que llama
     cursor = django_cursor.connection.cursor()
-    #Cursor que recibe
+    # Cursor que recibe
     out_cur = django_cursor.connection.cursor()
 
-    #Llamada al cursor 
-    cursor.callproc("SP_BUSCAR_CLIENTE", [out_cur,id])
+    # Llamada al cursor
+    cursor.callproc("SP_BUSCAR_CLIENTE", [out_cur, id])
 
-    #llenamos la lista
-    lista= []
+    # llenamos la lista
+    lista = []
     for fila in out_cur:
         lista.append(fila)
-    return lista 
+    return lista
+
 
 def buscar_mesa(id):
     django_cursor = connection.cursor()
 
-    #Cursor que llama
+    # Cursor que llama
     cursor = django_cursor.connection.cursor()
-    #Cursor que recibe
+    # Cursor que recibe
     out_cur = django_cursor.connection.cursor()
 
-    #Llamada al cursor 
-    cursor.callproc("SP_BUSCAR_MESA", [out_cur,id])
+    # Llamada al cursor
+    cursor.callproc("SP_BUSCAR_MESA", [out_cur, id])
 
-    #llenamos la lista
-    lista= []
+    # llenamos la lista
+    lista = []
     for fila in out_cur:
         lista.append(fila)
-    return lista 
+    return lista
 
 
-def reserva_modificar(request,id):
+def reserva_modificar(request, id):
 
     data = {
-        'mesas' : listado_mesas_disponibles(),
-        'empleados' : listado_empleados(),
-        'reserva' : buscar_reserva(id)
+        'mesas': listado_mesas_disponibles(),
+        'empleados': listado_empleados(),
+        'reserva': buscar_reserva(id)
     }
 
-    if request.method== 'POST':
+    if request.method == 'POST':
         fecha_reserva = request.POST.get('fecha_reserva')
         rut_emp = request.POST.get('empleado')
         rut_cli = request.POST.get('cliente')
@@ -215,46 +225,49 @@ def reserva_modificar(request,id):
         estado = request.POST.get('estado')
         cant = request.POST.get('cantP')
 
-        salida = modificar_reserva( id, fecha_reserva, rut_emp, rut_cli, origen, id_mesa, estado, cant)
+        salida = modificar_reserva(
+            id, fecha_reserva, rut_emp, rut_cli, origen, id_mesa, estado, cant)
 
         if salida == 1:
             data['mensaje'] = 'agregador correctamente'
         else:
             data['mensaje'] = 'no se pudo guardar'
 
-    return render(request, './reserva_modificar.html', data) 
+    return render(request, './reserva_modificar.html', data)
 
-def cliente_modificar(request,id):
+
+def cliente_modificar(request, id):
 
     data = {
 
-        'cliente' : buscar_cliente(id)
+        'cliente': buscar_cliente(id)
     }
 
-    if request.method== 'POST':
+    if request.method == 'POST':
         rut = request.POST.get('rutCli')
         dv = request.POST.get('dv')
         nombre = request.POST.get('nom')
         telefono = int(request.POST.get('telefono'))
         correo = request.POST.get('correo')
 
-        salida = modificar_cliente( rut,dv,nombre,telefono,correo)
+        salida = modificar_cliente(rut, dv, nombre, telefono, correo)
 
         if salida == 1:
             data['mensaje'] = 'agregado correctamente'
         else:
             data['mensaje'] = 'no se pudo guardar'
 
-    return render(request, './cliente_modificar.html', data) 
+    return render(request, './cliente_modificar.html', data)
+
 
 def reserva_crear(request):
 
     data = {
-        'mesas' : listado_mesas_disponibles(),
-        'empleados' : listado_empleados(),
+        'mesas': listado_mesas_disponibles(),
+        'empleados': listado_empleados(),
     }
 
-    if request.method== 'POST':
+    if request.method == 'POST':
         fecha_reserva = request.POST.get('fecha_reserva')
         hora_reserva = request.POST.get('hora_reserva')
         espacio = ' '
@@ -266,22 +279,24 @@ def reserva_crear(request):
         estado = request.POST.get('estado')
         cant = request.POST.get('cantP')
 
-        salida = crear_reserva( fecha_hora, rut_emp, rut_cli, origen, id_mesa, estado, cant)
+        salida = crear_reserva(fecha_hora, rut_emp, rut_cli,
+                               origen, id_mesa, estado, cant)
 
         if salida == 1:
             data['mensaje'] = 'agregador correctamente'
         else:
             data['mensaje'] = 'no se pudo guardar'
 
-    return render(request, './reserva_crear.html', data) 
+    return render(request, './reserva_crear.html', data)
+
 
 def cliente_crear(request):
 
     data = {
-        
+
     }
 
-    if request.method== 'POST':
+    if request.method == 'POST':
 
         rut = request.POST.get('rutCli')
         dv = request.POST.get('dv')
@@ -289,71 +304,80 @@ def cliente_crear(request):
         telefono = int(request.POST.get('telefono'))
         correo = request.POST.get('correo')
 
-        salida = crear_cliente( rut,dv,nombre,telefono,correo)
+        salida = crear_cliente(rut, dv, nombre, telefono, correo)
 
         if salida == 1:
             data['mensaje'] = 'agregador correctamente'
         else:
             data['mensaje'] = 'no se pudo guardar'
 
-    return render(request, './cliente_crear.html', data) 
+    return render(request, './cliente_crear.html', data)
 
 
-def crear_cliente( rut, dv, nom, telefono, correo):
+def crear_cliente(rut, dv, nom, telefono, correo):
     django_cursor = connection.cursor()
-    #Cursor que llama
+    # Cursor que llama
     cursor = django_cursor.connection.cursor()
     salida = cursor.var(cx_Oracle.NUMBER)
-    cursor.callproc('SP_AGREGAR_CLIENTE',[rut, dv, nom, telefono, correo ,salida ])
-    return salida.getvalue()
-
-def modificar_cliente( rut, dv, nom, telefono, correo):
-    django_cursor = connection.cursor()
-    #Cursor que llama
-    cursor = django_cursor.connection.cursor()
-    salida = cursor.var(cx_Oracle.NUMBER)
-    cursor.callproc('SP_MODIFICAR_CLIENTE',[rut, dv, nom, telefono, correo ,salida ])
-    return salida.getvalue()
-
-def eliminar_cliente( rut ):
-    django_cursor = connection.cursor()
-    #Cursor que llama
-    cursor = django_cursor.connection.cursor()
-    salida = cursor.var(cx_Oracle.NUMBER)
-    cursor.callproc('SP_ELIMINAR_CLIENTE',[rut, salida ])
+    cursor.callproc('SP_AGREGAR_CLIENTE', [
+                    rut, dv, nom, telefono, correo, salida])
     return salida.getvalue()
 
 
-def crear_reserva( fecha_reserva, rut_emp, rut_cli, origen, id_mesa, estado, cant):
+def modificar_cliente(rut, dv, nom, telefono, correo):
     django_cursor = connection.cursor()
-    #Cursor que llama
+    # Cursor que llama
     cursor = django_cursor.connection.cursor()
     salida = cursor.var(cx_Oracle.NUMBER)
-    cursor.callproc('SP_AGREGAR_RESERVA',[fecha_reserva,rut_emp ,rut_cli ,origen ,id_mesa ,estado ,cant ,salida ])
+    cursor.callproc('SP_MODIFICAR_CLIENTE', [
+                    rut, dv, nom, telefono, correo, salida])
     return salida.getvalue()
 
-def modificar_reserva( ID,fecha_reserva, rut_emp, rut_cli, origen, id_mesa, estado, cant):
+
+def eliminar_cliente(rut):
     django_cursor = connection.cursor()
-    #Cursor que llama
+    # Cursor que llama
     cursor = django_cursor.connection.cursor()
     salida = cursor.var(cx_Oracle.NUMBER)
-    cursor.callproc('SP_MODIFICAR_RESERVA',[ID, fecha_reserva,rut_emp ,rut_cli ,origen ,id_mesa ,estado ,cant ,salida ])
+    cursor.callproc('SP_ELIMINAR_CLIENTE', [rut, salida])
     return salida.getvalue()
 
-def eliminar_reserva( id ):
+
+def crear_reserva(fecha_reserva, rut_emp, rut_cli, origen, id_mesa, estado, cant):
     django_cursor = connection.cursor()
-    #Cursor que llama
+    # Cursor que llama
     cursor = django_cursor.connection.cursor()
     salida = cursor.var(cx_Oracle.NUMBER)
-    cursor.callproc('SP_ELIMINAR_RESERVA',[id , salida ])
+    cursor.callproc('SP_AGREGAR_RESERVA', [
+                    fecha_reserva, rut_emp, rut_cli, origen, id_mesa, estado, cant, salida])
     return salida.getvalue()
+
+
+def modificar_reserva(ID, fecha_reserva, rut_emp, rut_cli, origen, id_mesa, estado, cant):
+    django_cursor = connection.cursor()
+    # Cursor que llama
+    cursor = django_cursor.connection.cursor()
+    salida = cursor.var(cx_Oracle.NUMBER)
+    cursor.callproc('SP_MODIFICAR_RESERVA', [
+                    ID, fecha_reserva, rut_emp, rut_cli, origen, id_mesa, estado, cant, salida])
+    return salida.getvalue()
+
+
+def eliminar_reserva(id):
+    django_cursor = connection.cursor()
+    # Cursor que llama
+    cursor = django_cursor.connection.cursor()
+    salida = cursor.var(cx_Oracle.NUMBER)
+    cursor.callproc('SP_ELIMINAR_RESERVA', [id, salida])
+    return salida.getvalue()
+
 
 def cliente_eliminar(request, id):
 
     data = {
-     
+
     }
-   
+
     salida = eliminar_cliente(id)
 
     if salida == 1:
@@ -365,14 +389,15 @@ def cliente_eliminar(request, id):
 
     return render(request, './cliente_listar.html', data)
 
+
 def reserva_eliminar(request, id):
 
     data = {
-        'mesas' : listado_mesas_disponibles(),
-        'empleados' : listado_empleados(),
-     
+        'mesas': listado_mesas_disponibles(),
+        'empleados': listado_empleados(),
+
     }
-   
+
     salida = eliminar_reserva(id)
 
     if salida == 1:
@@ -382,18 +407,17 @@ def reserva_eliminar(request, id):
         data['mensaje'] = 'No se pudo borrar'
         data['reservas'] = listado_Reservas()
 
-    
-
     return render(request, './reserva_listado.html', data)
+
 
 def reserva_buscar(request):
 
     data = {
-        'mesas' : listado_mesas_disponibles(),
-        'empleados' : listado_empleados(),
+        'mesas': listado_mesas_disponibles(),
+        'empleados': listado_empleados(),
     }
 
-    if request.method== 'POST':
+    if request.method == 'POST':
         id = request.POST.get('rutCli')
         listadoReserva = listado_reservas_por_RUT(id)
 
@@ -403,67 +427,59 @@ def reserva_buscar(request):
         data['mensaje'] = 'no se pudo guardar'
         data['reservas'] = listado_Reservas()
 
-    return render(request, './reserva_listado.html', data) 
+    return render(request, './reserva_listado.html', data)
 
 
-
-
-#Listado reservas cursor
+# Listado reservas cursor
 def listado_Reservas():
     django_cursor = connection.cursor()
-    #Cursor que llama
+    # Cursor que llama
     cursor = django_cursor.connection.cursor()
-    #Cursor que recibe
+    # Cursor que recibe
     out_cur = django_cursor.connection.cursor()
 
     cursor.callproc("SP_LISTAR_RESERVAS", [out_cur])
 
-    lista= []
+    lista = []
     for fila in out_cur:
         lista.append(fila)
-    return lista 
+    return lista
 
 
-
-#Vista listado reserva
+# Vista listado reserva
 def reserva_listado(request):
     data = {
-        'reservas' : listado_Reservas(),
-        'base' : listado_Reservas()
+        'reservas': listado_Reservas(),
+        'base': listado_Reservas()
     }
 
-    if request.method== 'POST':
+    if request.method == 'POST':
         id = request.POST.get('rutcli')
         listadofiltro = listado_reservas_por_RUT(id)
         data['reservas'] = listadofiltro
 
-    return render(request,'./reserva_listado.html',data)
+    return render(request, './reserva_listado.html', data)
 
 
 def horario_mesa(request, id):
     data = {
-        'horarios' :    listado_Horario(id),
-        'mesa' :buscar_mesa(id)
+        'horarios':    listado_Horario(id),
+        'mesa': buscar_mesa(id)
     }
 
-    return render(request,'./mesa_horario.html',data)
+    return render(request, './mesa_horario.html', data)
+
 
 def listado_Horario(id):
     django_cursor = connection.cursor()
-    #Cursor que llama
+    # Cursor que llama
     cursor = django_cursor.connection.cursor()
-    #Cursor que recibe
+    # Cursor que recibe
     out_cur = django_cursor.connection.cursor()
 
     cursor.callproc("SP_LISTAR_HORARIO", [out_cur, id])
 
-    lista= []
+    lista = []
     for fila in out_cur:
         lista.append(fila)
-    return lista 
-
-
-
-
-
-
+    return lista
